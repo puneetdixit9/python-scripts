6b716f5cf6fef99b68cb993d3cd969445cf5c317
Puneet Dixit <puneet.dixit@statusneo.com>
2023-05-25 16:50:13 +0530

    Desc: post request added

--- test_api_load.py
+++ test_api_load.py
@@ -1,6 +1,6 @@
-from multiprocessing import Process
-import requests
 import time
+import requests
+from multiprocessing import Process
 
 
 def call_api(method: str, url: str, headers: dict = dict, data: dict = dict, json: dict = dict):
@@ -20,23 +20,40 @@ def call_api(method: str, url: str, headers: dict = dict, data: dict = dict, jso
     return response
 
 
-request_method = "get"
-request_url = "http://127.0.0.1:5000/wmp/demands/1?start_date=2023-05-24&end_date=2023-05-31"
+# request_method = "get"
+# request_url = "http://127.0.0.1:5000/wmp/demands/1?start_date=2023-05-24&end_date=2023-05-31"
+
+request_method = "post"
+request_url = "http://127.0.0.1:5000/wmp/warehouses"
+
 request_headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NTAwMTc4MCwianRpIjoiNzk1YmVhZTktMjYwNC00OWRhLThkNTQtOTY2YmQ0Y2YyNzFjIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ1c2VyX2lkIjo0LCJyb2xlIjpudWxsfSwibmJmIjoxNjg1MDAxNzgwLCJleHAiOjE2ODUwODgxODB9.FhKQRXkCCx7v_pY3f4fvFbE_I3DDmtHoZWZMbdUUlUA"
 }
+data = {}
+json = {
+    "warehouses": [
+        {
+            "name": "Warehouse F",
+            "description": "Warehouse F"
+        }
+    ]
+}
+
+
 number_of_api_calls = 100
+
 
 process_list = []
 
-start_time = time.time()
+start_time = time.time()  # Start time
 
 for i in range(number_of_api_calls+1):
-    p = Process(target=call_api, args=(request_method, request_url, request_headers, {}, {}))
+    p = Process(target=call_api, args=(request_method, request_url, request_headers, data, json))
     p.start()
     process_list.append(p)
 
 for p in process_list:
     p.join()
 
-print(f"Total time to call {request_url}  {number_of_api_calls} times :-> {time.time() - start_time}")
+# End time
+print(f"Total time to call {request_url} {number_of_api_calls} times :-> {time.time() - start_time}")
